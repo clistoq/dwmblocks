582ca026342ee92903d04dda5b4b585deaf12f2b
Dawid Butrykowski <dawid.butrykowski@mindboxgroup.com>
2020-04-21 17:29:51 +0200

    style(config): Remove additional slash

--- config.mk
+++ config.mk
@@ -1,6 +1,6 @@
 VERSION = 1.0
 
-PREFIX    	?= /usr/local/
+PREFIX    	?= /usr/local
 
 CFLAGS  = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os `pkg-config --cflags x11`
 LDFLAGS = `pkg-config --libs x11`
