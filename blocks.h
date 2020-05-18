145aec5263328fb4e43651dbac7c7ed45ec33774
Dawid Butrykowski <dawid.butrykowski@mindboxgroup.com>
2020-05-18 22:57:20 +0200

    refactor(view): Change icons

--- blocks.h
+++ blocks.h
@@ -2,7 +2,7 @@ static const Block blocks[] = {
     /* Icon         Command                    Interval    Signal */
 //	{ "\uF001 ",    "/opt/dwmblocks/music",    5,          0      },
     { "\uE266 ",    "/opt/dwmblocks/cpu",      5,         0      },
-    { "\uF1C0 ",    "/opt/dwmblocks/memory",   5,         0      },
+    { "\uF85A ",    "/opt/dwmblocks/memory",   5,         0      },
     { "\uF028 ",    "/opt/dwmblocks/volume",   1,          0      },
     { "\uF80B ",    "/opt/dwmblocks/keyboard", 0,          0      },
 	  { "",						"/opt/dwmblocks/battery",  30,         0      },
